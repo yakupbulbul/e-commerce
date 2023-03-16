c53828c8e7fafff48339c63ec1af0f79f077839c
Türcan <73202042+Turcan-vs@users.noreply.github.com>
2023-03-16 16:54:21 +0300

    .

--- client/src/Pages/Error/errorPage.jsx
+++ client/src/Pages/Error/errorPage.jsx
@@ -6,7 +6,7 @@ const errorPage = () => {
   return (
     <div className='not-found'>
        <h1>Sayfa bulunamadı</h1>
-      <p>Arzu edilen sayfa hiç bir zaman bulunamayacak. İyi günler.</p>
+      <p>Arzu edilen sayfa hiçbir zaman bulunamayacak. İyi günler.</p>
     </div>
   )
 }
