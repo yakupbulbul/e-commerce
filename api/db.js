d3bbed88699124d38879d3a1f367862456622ec8
yakupbulbul <81179438+yakupbulbul@users.noreply.github.com>
2023-04-04 20:37:11 +0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql"
 
 export const db = mysql.createConnection({
     host:"localhost",
-    user: "root",
-    password: "92587aykn",
-    database: "test"
+    user: "",
+    password: "",
+    database: ""
 })
